f1664ef53bbac812c6cf36b23b9976af388b22e2
Stephan Lachnit <stephanlachnit@debian.org>
2025-04-10 16:58:50 +0200

    ENH: add function to read single scalar

--- aidatlu/test/test_tlu.py
+++ aidatlu/test/test_tlu.py
@@ -46,7 +46,9 @@ def test_check_ups():
         assert TLU.get_run_active() == 0
         assert TLU.get_event_fifo_fill_level() == -1
         assert TLU.get_timestamp() == -0x100000001
-        assert TLU.get_scalar() == (-1, -1, -1, -1, -1, -1)
+        assert TLU.get_scalars() == [-1, -1, -1, -1, -1, -1]
+        with pytest.raises(ValueError):
+            TLU.get_scalar(6)
     else:
         TLU.set_event_fifo_csr(0)
         assert TLU.get_event_fifo_csr() == 3
