d7900876e661e2fd7b5e2817e583a6739806f530
Stephan Lachnit <stephanlachnit@debian.org>
2025-06-25 10:04:09 +0200

    Use DataSenderArgumentParser

--- aidatlu/constellation/__main__.py
+++ aidatlu/constellation/__main__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from constellation.core.logging import setup_cli_logging
-from constellation.core.satellite import SatelliteArgumentParser
+from constellation.core.datasender import DataSenderArgumentParser
 
 from .aidatlu_satellite import AidaTLU
 
@@ -8,7 +8,7 @@ from .aidatlu_satellite import AidaTLU
 def main(args=None):
     """Controlling of a Satellite for the AIDA-2020 TLU"""
 
-    parser = SatelliteArgumentParser(description=main.__doc__)
+    parser = DataSenderArgumentParser(description=main.__doc__)
     args = vars(parser.parse_args(args))
 
     # set up logging
